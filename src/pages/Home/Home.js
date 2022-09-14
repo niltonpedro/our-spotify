a0b8d840a9b2ebaa83867f3e526f748187b87e8f
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2022-09-13 22:03:04 -0300

    card component added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,42 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
 function Home() {
     return (
         <>
-            <h1>Home Page</h1>
+        <div className="content">
+            <div className="card1">
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                    <CardMedia
+                    component="img"
+                    height="140"
+                    image="../../assets/homepage/bilie.jpg"
+                    alt="green iguana"
+                    />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Listen Bilie Elish
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000
+                            species, ranging across all continents except Antarctica
+                        </Typography>
+                        </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                    Share
+                    </Button>
+                </CardActions>
+                </Card>
+            </div>
+
+        </div>
+            
         </>
     );
   }
