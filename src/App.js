b1e4fd9c7dc04314fd223a4fa65993f3793d4b78
Glauber de Sousa Fernandes <glauber.fernandes@tbmtextil.com.br>
2022-09-14 08:55:51 -0300

    Adicionado o Menu no arquivo App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Faq from './pages/Faq/Faq'
+import MenuHeader from './components/Menu/MenuHeader';
 
 function App() {
   return (
     <>
+    <MenuHeader></MenuHeader>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/faq' element={<Faq />}  />
