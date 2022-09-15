3e4c043d60e17a73d84a401cf2a25e49af07de67
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2022-09-15 17:49:02 -0300

    adicionando todos albuns na pagina inicial

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import Card from '@mui/material/Card';
+import './Home.css';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
@@ -7,159 +8,271 @@ import { Button, CardActionArea, CardActions } from '@mui/material';
 function Home() {
     return (
         <>
-        <div className="content">
-            <div className="card1">
-            <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image="../../assets/homepage/bilie.jpg"
-                    alt="Discos"
-                    />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Listen Bilie Elish
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Bilie is the best artist int the rock
-                        </Typography>
-                        </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Share
-                    </Button>
-                </CardActions>
-                </Card>
+            <div className="content" id='BoxCards1'>
+                <div className="card1">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/bilie.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
 
-                <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image="../../assets/homepage/AcDc.jpg"
-                    alt="green iguana"
-                    />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            AcDc
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
-                        </Typography>
-                        </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Share
-                    </Button>
-                </CardActions>
-                </Card>
+                <div className="card2">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/Yoga.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
 
-                <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image="../../assets/homepage/Cozinhando.jpeg"
-                    alt="green iguana"
-                    />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Cozinhando e cantando
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
-                        </Typography>
-                        </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Share
-                    </Button>
-                </CardActions>
-                </Card>
+                <div className="card3">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/AcDc.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
 
-                <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image="../../assets/homepage/Discos.jpg"
-                    alt="green iguana"
-                    />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Moda Discol....
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
-                        </Typography>
-                        </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Share
-                    </Button>
-                </CardActions>
-                </Card>
+                <div className="card4">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/AcDc.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
 
-                <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image="../../assets/homepage/FocoNoTrabalho.jpeg"
-                    alt="green iguana"
-                    />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Foco no trabalho.
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
-                        </Typography>
-                        </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Share
-                    </Button>
-                </CardActions>
-                </Card>
+                <div className="card5">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/AcDc.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
 
-                <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image="../../assets/homepage/red_Hot.jpg"
-                    alt="green iguana"
-                    />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                        Red Hot Chili Peppers Californication
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            Lizards are a widespread group of squamate reptiles, with over 6,000
-                            species, ranging across all continents except Antarctica
-                        </Typography>
-                        </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                    Share
-                    </Button>
-                </CardActions>
-                </Card>
             </div>
 
-        </div>
+            <div className="content" id='BoxCards1'>
+                <div className="card1">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/bilie.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
+
+                <div className="card2">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/Yoga.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
+
+                <div className="card3">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/AcDc.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
+
+                <div className="card4">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/AcDc.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
+
+                <div className="card5">
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardActionArea>
+                            <CardMedia
+                            component="img"
+                            height="140"
+                            image={require('../../assets/homepage/AcDc.jpg')}
+                            alt="Bilie album"
+                            />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Listen Bilie Elish
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Bilie is the best artist int the rock
+                                </Typography>
+                                </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                            Share
+                            </Button>
+                        </CardActions>
+                        </Card>
+                </div>
+
+            </div>
             
         </>
     );
