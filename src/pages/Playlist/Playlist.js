c7c0fb498abdbb0aadd866bf3bf7bb3d7ad08505
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2022-10-04 19:47:54 -0300

    Style of playlsit page

--- src/pages/Playlist/Playlist.js
+++ src/pages/Playlist/Playlist.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import './Playlist.css'
+import bilie from '../../assets/homepage/bilie.jpg';
 
 class Playlist extends Component {
     render() {
         return (
-            <div>
-                <h1>PlaylistHover</h1>
+            <div className='background' >
+
+                <div className="pictureAndTitle">
+                    <div className="content">
+                        <img src={bilie} alt="bilieAlbum" />
+                        <span>This is Bilie Elish</span>
+                    </div>
+                </div>
+
+                <div className="listOfMusics">
+                    <ul>
+                        <li>
+                            <div className="blocos">
+                                <div className="comandsMusic">
+                                    <button>Pause</button>
+                                    <button>Play</button>
+                                </div>
+                                <div className="MusicInfo">
+                                    <div className="title">
+                                        <span>All good girls</span>
+                                    </div>
+                                    <div className="artista">
+                                        <span>Bilie Elish</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+
+                </div>
             </div>
         );
     }
