130a6d3687e62c798b4cd288233c6aee7322b6f3
Nilton Pedro <niltonpedro@MacBook-Air-de-Nilton.local>
2022-10-13 21:48:52 -0300

    merge all branches

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ import RegistrationForm from "./pages/RegistrationForm/RegistrationForm";
 import Navbar from './components/Menu/Navbar';
 import Playlist from './pages/Playlist/Playlist';
 
-<<<<<<< HEAD
-import Footer from './components/Footer/Footer';
-=======
-import Playlist from './pages/Playlist/Playlist';
->>>>>>> main
-
 function App() {
   return (
     <>
@@ -21,14 +15,7 @@ function App() {
         <Route path='/faq' element={<Faq />} />
         <Route path='/playlist' element={<Playlist />} />
         <Route path='/Inscrever-se' element={<RegistrationForm/>} />
-
-<<<<<<< HEAD
-        <Route path='/Inscrever-se' element={<RegistrationForm/>} />
-=======
-        <Route path='/playlist' element={<Playlist />} />
->>>>>>> main
       </Routes>
-      <Footer />
     </>
   );
 }
